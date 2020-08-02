0895402b3d6d7987fe8f5bb7af25a81d37baee71
lucasfrosty <lucas_frosty@hotmail.com>
2020-08-02 16:10:58 -0400

    fixing import typo

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,5 @@ export {Layout} from './layout';
 export {SEO} from './seo';
 export {Header} from './header';
 export {Circle} from './circle';
-export {Social} from './Social';
+export {Social} from './social';
 export {LanguageSwitcherButton} from './language-switcher-button';
