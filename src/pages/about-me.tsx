01dd8d597d8e7c1f0581173ec4aeba6a33593fb3
lucasfrosty <lucas_frosty@hotmail.com>
2020-08-02 21:30:41 -0400

    fixing text description breakpoint

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { useTranslation, Trans } from "react-i18next"
-import Img, { FixedObject } from 'gatsby-image';
+import Img, { FixedObject } from "gatsby-image"
 import styled from "styled-components"
 
 import { Link, graphql, useStaticQuery } from "gatsby"
 import { colors, breakPointsInPx } from "../styles"
 import { Routes } from "../utilities/routes"
-import { SEO } from "../components";
+import { SEO } from "../components"
 
 const SectionWrapper = styled.div`
   display: flex;
@@ -21,7 +21,7 @@ const SectionWrapper = styled.div`
 
 const DescriptionWrapper = styled.div`
   width: 100%;
-  
+
   @media only screen and (min-width: ${breakPointsInPx.aboutMe}px) {
     max-width: 500px;
   }
@@ -42,7 +42,7 @@ const ImageWrapper = styled.div`
     margin-bottom: 40px;
   }
 `
-const RoundedImage = styled((props) => <Img {...props} />)`
+const RoundedImage = styled(props => <Img {...props} />)`
   border-radius: 100%;
   border: 3px solid white;
   box-shadow: 14px 19px 0px 5px ${colors.primary};
@@ -67,25 +67,27 @@ const NameHeader = styled.h1`
 
 const Text = styled.p<{caption?: boolean}>`
   color: #464646;
+  font-size: ${props => (props.caption ? 15 : 18)}px;
   margin: 0;
-  font-size: ${props => props.caption ? 15 : 18}px;
 `
 interface SkillLogo {
-  fixed: FixedObject;
-  width?: number;
-  height?: number;
-  marginLeft?: number;
-  alt: string;
+  fixed: FixedObject
+  width?: number
+  height?: number
+  marginLeft?: number
+  alt: string
 }
-
-const LogoImg = styled.img<Omit<SkillLogo, "src">>`
-  width: ${props => `${props.width}px`};
-  height: ${props => `${props.height}px`};
-  margin-left: ${props => `${props.marginLeft}px`};
-`
 
 const SecondHeader = styled.h2`
   color: ${colors.primary};
+`
+
+const SpacedComponent = styled.div<{ margin: string, atBreakpoint?: number }>`
+  margin: ${props => props.atBreakpoint ? 0 : (props.margin || 0)};
+
+  @media only screen and (max-width: ${props => props.atBreakpoint}px) {
+    margin: ${props => props.margin};
+  }
 `
 
 export const query = graphql`
@@ -142,14 +144,37 @@ export const query = graphql`
 
 export default function AboutMe() {
   const { t, i18n } = useTranslation()
-  const {ReactLogo, ReduxLogo, GraphQLLogo, ApolloLogo, RailsLogo, Me} = useStaticQuery(query)
+  const {
+    ReactLogo,
+    ReduxLogo,
+    GraphQLLogo,
+    ApolloLogo,
+    RailsLogo,
+    Me,
+  } = useStaticQuery(query)
 
   const skillListLogos: SkillLogo[] = [
-    { fixed: ReactLogo.childImageSharp.fixed, marginLeft: 0, alt: "React.js logo" },
-    { fixed: ReduxLogo.childImageSharp.fixed, marginLeft: 13, alt: "Redux logo" },
+    {
+      fixed: ReactLogo.childImageSharp.fixed,
+      marginLeft: 0,
+      alt: "React.js logo",
+    },
+    {
+      fixed: ReduxLogo.childImageSharp.fixed,
+      marginLeft: 13,
+      alt: "Redux logo",
+    },
     { fixed: GraphQLLogo.childImageSharp.fixed, alt: "GraphQL logo" },
-    { fixed: ApolloLogo.childImageSharp.fixed, marginLeft: 11, alt: "Apollo logo" },
-    { fixed: RailsLogo.childImageSharp.fixed, marginLeft: 8, alt: "Ruby on Rails logo" },
+    {
+      fixed: ApolloLogo.childImageSharp.fixed,
+      marginLeft: 11,
+      alt: "Apollo logo",
+    },
+    {
+      fixed: RailsLogo.childImageSharp.fixed,
+      marginLeft: 8,
+      alt: "Ruby on Rails logo",
+    },
   ]
 
   const shopifyLink =
@@ -159,44 +184,44 @@ export default function AboutMe() {
 
   return (
     <>
-    <SEO title={t('aboutMePageTitle')} />
-    <SectionWrapper>
-      <DescriptionWrapper>
-        <NameHeader>Lucas Ferreira</NameHeader>
-        <Text>{t("description")}</Text>
-        <Text>
-          <Trans i18nKey="description2">
-            I'm from Brazil but currently i'm based in Ottawa, Canada working at <Link target="_blank" to={shopifyLink}>Shopify</Link>.
-          </Trans>
-        </Text>
-        <Text style={{ marginTop: 30 }}>{t("description3")}</Text>
-
-        <div style={{ marginTop: 80 }}>
-          <SecondHeader>{t("skills")}</SecondHeader>
-          <div>
+      <SEO title={t("aboutMePageTitle")} />
+      <SectionWrapper>
+        <DescriptionWrapper>
+          <NameHeader>Lucas Ferreira</NameHeader>
+          <Text>{t("description")}</Text>
+          <SpacedComponent margin="25px 0 0 0" atBreakpoint={550}>
+            <Text>
+              <Trans i18nKey="description2">
+                I'm from Brazil but currently i'm based in Ottawa, Canada working
+                at <Link target="_blank" to={shopifyLink}>Shopify</Link>.
+              </Trans>
+            </Text>
+          </SpacedComponent>
+          <SpacedComponent margin="30px 0 0 0"><Text>{t("description3")}</Text></SpacedComponent>
+          <SpacedComponent margin="80px 0 0 0">
+            <SecondHeader>{t("skills")}</SecondHeader>
             <div>
-              {skillListLogos.map(
-                ({ fixed, marginLeft = 15, alt }) => (
-                  <Img alt={alt} fixed={fixed} style={{marginLeft}} />
-                )
-              )}
+              <div>
+                {skillListLogos.map(({ fixed, marginLeft = 15, alt }) => (
+                  <Img alt={alt} fixed={fixed} style={{ marginLeft }} />
+                ))}
+              </div>
+              <SpacedComponent margin="25px 0 0 0">
+                <Text caption>
+                  <Trans i18nKey="workDescription">
+                    Those are the main technologies i'm using right now, but i'm
+                    always open to learn and explore new ones. For more
+                    information check out <Link to={Routes.Work}>Work</Link>.
+                  </Trans>
+                </Text>
+              </SpacedComponent>
             </div>
-            <div style={{marginTop: 25}}>
-              <Text caption>
-                <Trans i18nKey="workDescription">
-                  Those are the main technologies i'm using right now, but i'm
-                  always open to learn and explore new ones. For more
-                  information check out <Link to={Routes.Work}>Work</Link>.
-                </Trans>
-              </Text>
-            </div>
-          </div>
-        </div>
-      </DescriptionWrapper>
-      <ImageWrapper>
-        <RoundedImage fixed={Me.childImageSharp.fixed} alt="My photo" />
-      </ImageWrapper>
-    </SectionWrapper>
+          </SpacedComponent>
+        </DescriptionWrapper>
+        <ImageWrapper>
+          <RoundedImage fixed={Me.childImageSharp.fixed} alt="My photo" />
+        </ImageWrapper>
+      </SectionWrapper>
     </>
   )
 }
